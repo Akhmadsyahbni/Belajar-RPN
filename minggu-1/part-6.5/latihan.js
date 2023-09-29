1a748c40a40bbd592f1884eb82955ca3bd504e1e
Baban <akhmad.syahbni@gmail.com>
2023-09-29 20:55:46 +0700

    menambhakn latihan loop

---
+++ minggu-1/part-6.5/latihan.js
@@ -0,0 +1,41 @@
+// For loop
+
+// for(let i =1; i <=10; i++){
+//     console.log(i); // intinya pada perulangan ketika ingin menambah / increment maka harus ke kiri panah nya atau lebih simple nya kurang dari
+// }
+
+// console.log('')
+
+// for(let j = 10; j >=1; j--){
+//     console.log(j); // intinya pada perulangan ketika ingin mundur / decrement maka harus ke kanan panah nya atau lebih simple nya lebih dari
+// }
+
+// console.log('');
+
+// Perulangan array menggunakan for
+
+// let foodFavorite = ['nasi padang','ciken','telor','bakso'];
+
+// for(let myFavorite of foodFavorite){
+//     console.log(myFavorite); // buat variavel baru untuk menampung hasil dari array sevbenarnya
+// }
+
+// console.log ('');
+
+// perulangan menggunakan while
+// while lebih cocok digunakan pada kasus di mana kita tidak tahu pasti berapa banyak perulangan yang diperlukan.
+let i = 1;
+while (i <=1){
+    console.log(i); //while ini akan di jalankan dahulu lalu baru memulai perulanganya
+    i++;
+}
+
+console.log('');
+
+let j = 1;
+
+do {
+    console.log(i);
+    i++;
+}while(i <=50); // sedangkan do-while akan mengevaluasi boolean expression setelah blok kodenya berjalan. Ini artinya kode di dalam do-while akan dijalankan setidaknya satu kali.
+
