25dc47705a26edb6b56232258a12592a10487b49
Baban <akhmad.syahbni@gmail.com>
2023-10-02 13:36:15 +0700

    menambah pengertian spread operator minggu 2 file latihan

--- minggu-1/part-5/latihan.js
+++ minggu-1/part-5/latihan.js
@@ -1,19 +1,36 @@
 let uts = 42;
 
-if(uts >= 90){
+if (uts >= 90) {
     console.log(`kamu mendaptkan nilai uts A karena nilaimu ${uts}`);
-}else if(uts >=80 && uts <=89){
+} else if (uts >= 80 && uts <= 89) {
     console.log(`kamu mendaptkan nilai uts B karena nilaimu ${uts}`);
-}else if(uts >=70 && uts <=69){
+} else if (uts >= 70 && uts <= 69) {
     console.log(`kamu mendaptkan nilai uts C karena nilaimu ${uts}`);
-}else if(uts >=60 && uts <=59){
+} else if (uts >= 60 && uts <= 59) {
     console.log(`kamu mendaptkan nilai uts D karena nilaimu ${uts}`);
-}else if(uts >=40 && uts <=49){
+} else if (uts >= 40 && uts <= 49) {
     console.log(`kamu mendaptkan nilai uts D karena nilaimu ${uts}`);
-}else {
+} else {
     console.log("Anda bot rpn kali ya tidak ada nilai kriteria sama sekali");
 }
 
 console.log('');
 
+console.log("Belaajar Nested If else");
 
+let uas = "string";
+
+console.log(`Nilai uas kamu adalah ${uas}`);
+
+if (typeof (uas === "number")) {
+    console.log(`nilai ${uas} bertipe number`);
+    if (uas >= 90) {
+        console.log(`Selmat anda lulusan terbaik`);
+    } else {
+        console.log('anda bukan lulusan terbaik');
+    }
+} else if (typeof uas === "string") {
+    console.log("uas bertipe string")
+} else {
+    console.log("tidak ada")
+}
