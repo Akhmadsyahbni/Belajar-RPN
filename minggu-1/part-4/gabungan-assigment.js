ddc761dab1291ef432137e5cd1e7ddb2ea3075b0
Baban <akhmad.syahbni@gmail.com>
2023-10-02 11:34:28 +0700

    menambah file gabungan assigment di minggu-1 part-5

---
+++ minggu-1/part-4/gabungan-assigment.js
@@ -0,0 +1,17 @@
+/*Perintah asignment seperti a = a + 10, atau b = b + " JavaScript" sangat sering digunakan.
+Karena itu pula tersedia penulisan singkat dari perintah ini, yang bisa disebut sebagai operator
+gabungan assignment.*/
+
+let a = 12;
+a += 10;
+a += 8;
+
+let b = 5;
+b *= 5;
+b *= 2;
+b *= 2;
+b *= 5;
+
+console.log(a);
+
+console.log(b);
