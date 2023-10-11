387d982f1ecc64919cd9d2530d07cbecea1e23dc
Baban <akhmad.syahbni@gmail.com>
2023-10-11 11:01:15 +0700

    menambahkan file escape character

---
+++ minggu-1/part-4/escape-character.js
@@ -0,0 +1,6 @@
+/* Escpe Character adalah cara memasukan karakter khusus ke dalam string untuk menggunakan 
+karakter ini adalah backslash (\) contohnya seperti ini */
+
+let sayHello = "Hallo, Perkenalkan Saya \"Akhmad Syahbani\"";
+
+console.log(sayHello);
