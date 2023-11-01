45cde95729fca9133f92dfa7189fcc490d6210dc
Baban <akhmad.syahbni@gmail.com>
2023-11-01 14:34:38 +0700

    menambah file comment di folder fundamental-js

---
+++ fundamental-js/comment.js
@@ -0,0 +1,6 @@
+//Salah satu instruksi yang penting adalah memberi tahu komputer untuk mengabaikan perintah yang kita tulis. 
+//Instruksi yang ditulis dalam suatu program tetapi tidak dijalankan oleh komputer disebut “comments”
+/*Sebuah komentar akan dilewatkan oleh interpreter atau compiler, sehingga tidak akan memengaruhi alur program yang kita tulis. Komentar ini bisa digunakan sebagai dokumentasi 
+atau penjelasan dari kode yang kita tulis.*/
+
+//console.log("Hello world");
