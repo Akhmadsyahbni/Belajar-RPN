aadb18f9d2b5d61da9006eb369da6edcfd151654
Baban <akhmad.syahbni@gmail.com>
2023-11-06 13:37:45 +0700

    menmabah file pengertian function

---
+++ function-dasar/closure.js
@@ -0,0 +1,12 @@
+/*  Suatu fungsi yang dapat mengakses variabel di dalam lexical scope-nya disebut dengan closure. Lexical scope berarti pada sebuah fungsi bersarang,
+ fungsi yang berada di dalam memiliki akses ke variabel di lingkup induknya.*/
+ function init() {
+    const name = 'Obi Wan';   // Variabel lokal di dalam scope fungsi init
+      
+    function greet() {      // Inner function, merupakan contoh closure
+      console.log(`Halo, ${name}`);   // Memanggil variabel yang dideklarasikan di parent function
+    }
+  
+    greet();
+  }
+  init();
---
+++ function-dasar/latihan.js
@@ -0,0 +1,36 @@
+/*    1. Buatlah fungsi bernama minimal dengan ketentuan berikut:
+ *    - Menerima dua buah argumen number, a dan b.
+ *    - Mengembalikan nilai terkecil antara a atau b.
+ *    - Bila nilai keduanya sama, maka kembalikan dengan nilai a
+ * contoh:
+ *    minimal(1, 4) // 1
+ *    minimal(3, 2) // 2
+ *    minimal(3, 3) // 3
+ */
+
+function minimal(a,b){
+    if(a < b){
+        return a;
+    }else if (a > b){
+        return b;
+    }else if(a == b){
+        return a;
+    }else{
+        return console.log('eror');
+    }
+}
+
+console.log(minimal(1,4));
+console.log(minimal(3,2));
+console.log(minimal(3,3));
+
+console.log('');
+
+function power(a,b){
+    return a ** b;
+}
+console.log(power(7,3));
+console.log(power(3,3));
+console.log(power(4,0.5));
+
+console.log('')
