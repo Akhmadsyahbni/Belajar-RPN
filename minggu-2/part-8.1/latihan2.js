9f93a87cda299a9e4fb8ed34c3d7f7bfe3f416e4
Baban <akhmad.syahbni@gmail.com>
2023-11-11 13:49:59 +0700

    menambah file latihan2

---
+++ minggu-2/part-8.1/latihan2.js
@@ -0,0 +1,9 @@
+// latihan dari yt kang dea afrizal
+const myArray = [3,5,12];
+let result = 0;
+
+for(let i = 0; i < myArray.length; i++){
+    result += myArray[i];
+}
+console.log(myArray);
+console.log(`hasil dari ${result} karena ${myArray[0]} + ${myArray[1]} + ${myArray[2]} = ${result} `);
